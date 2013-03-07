ea4c83e2f254adefa992d72d1f4d3c89453227b4
Thimios <thimios@wantedpixel.com>
2013-03-07 11:32:38 +0100

    uberspacified

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,37 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# include uberspacify base recipes
+require 'uberspacify/base'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+# comment this if you don't use MySQL
+require 'uberspacify/mysql' 
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+# the Uberspace server you are on
+server 'aries.uberspace.de', :web, :app, :db, :primary => true
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+# your Uberspace username
+set :user, 'wantedsi'
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+# a name for your app, [a-z0-9] should be safe, will be used for your gemset,
+# databases, directories, etc.
+set :application, 'paypademo'
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+# the repo where your code is hosted
+set :scm, :git
+set :repository, 'https://thimios@bitbucket.org/thimios/gigflip-paypal-demo.git'
+
+# optional stuff from here
+
+# By default, your app will be available in the root of your Uberspace. If you
+# have your own domain set up, you can configure it here
+set :domain, 'paypaldemo.wantedpixel.com'
+
+# By default, uberspacify will generate a random port number for Passenger to
+# listen on. This is fine, since only Apache will use it. Your app will always
+# be available on port 80 and 443 from the outside. However, if you'd like to
+# set this yourself, go ahead.
+# set :passenger_port, 55555
+
+# By default, Ruby Enterprise Edition 1.8.7 is used for Uberspace. If you
+# prefer Ruby 1.9 or any other version, please refer to the RVM documentation
+# at https://rvm.io/integration/capistrano/ and set this variable.
+set :rvm_ruby_string, 'rvm 1.9.3@gigflip-paypal-demo'
+
