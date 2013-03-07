04faabd8ef8c7adf88f6f22295294b8575bd7efa
Thimios <thimios@wantedpixel.com>
2013-03-07 11:27:25 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paypaldemo::Application.config.secret_token = 'cf47a07f5e387a7ca59790a8f870e23866f523e0cdaf311d7416fadbfcef15ec61fb05ed3535ef25a13d78209762820a695fd0d44270a4a5f631e7c4cc7fe11e'
